02bcee1e5a25b3a1aa994d380312d569827c1a78
Mantshakhan10 <mantshakhan5020@gmail.com>
2026-08-12 16:56:44 +0530

    Completed Day-7

--- Day-7-TypeScript/student.js
+++ Day-7-TypeScript/student.js
@@ -1,14 +1,14 @@
-"use strict";
-const student = {
+const studentData = {
     name: "Mantsha Khan",
     age: 20,
     course: "BCA",
     skills: ["HTML", "CSS", "JavaScript", "React"]
 };
-function displayStudent(student) {
+function displayStudentInfo(student) {
     console.log("Name:", student.name);
     console.log("Age:", student.age);
     console.log("Course:", student.course);
     console.log("Skills:", student.skills.join(", "));
 }
-displayStudent(student);
+displayStudentInfo(studentData);
+export {};
--- Day-7-TypeScript/student.ts
+++ Day-7-TypeScript/student.ts
@@ -5,18 +5,20 @@ interface Student {
     skills: string[];
 }
 
-const student: Student = {
+export {};
+
+const studentData: Student = {
     name: "Mantsha Khan",
     age: 20,
     course: "BCA",
     skills: ["HTML", "CSS", "JavaScript", "React"]
 };
 
-function displayStudent(student: Student): void {
+function displayStudentInfo(student: Student): void {
     console.log("Name:", student.name);
     console.log("Age:", student.age);
     console.log("Course:", student.course);
     console.log("Skills:", student.skills.join(", "));
 }
 
-displayStudent(student);
+displayStudentInfo(studentData);
